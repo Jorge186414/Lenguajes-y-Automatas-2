1e96b9d991ad205103870dc20dd920b99f48cc60
Jorge Miranda <jorgaismiranda@gmail.com>
2024-04-02 15:32:10 -0600

    Se declararon variables para el procedimiento y ciclo while para la extraccion de nodos

--- Vuelos.py
+++ Vuelos.py
@@ -2,4 +2,13 @@
 from Arbol import Nodo
 
 def buscar_solucion_BFS(conexiones, estado_inicial, solucon):
-    return 
+    solucionado = False
+    nodos_Visitados = []
+    nodos_Frontera = []
+    nodo_inicial = Nodo(estado_inicial)
+    nodos_Frontera.append(nodo_inicial)
+    while (not solucionado) and len(nodos_Frontera) !=0:
+        Nodo = nodos_Frontera[0]
+        
+        # Extraer nodo y añadirlos a visitados
+        nodos_Visitados.append(nodos_Frontera.pop(0))
